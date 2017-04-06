f5d26dd9672241634b3fb081386f8c1bc8c86f21
Larry Lade <lplade@users.noreply.github.com>
2017-04-06 00:07:46 -0500

    extend to use RDS

--- LMNOPsite/LMNOPsite/settings.py
+++ LMNOPsite/LMNOPsite/settings.py
@@ -74,17 +74,28 @@ WSGI_APPLICATION = 'LMNOPsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lmnop',
-        'USER': 'lmnop',
-        'PASSWORD': os.environ['POSTGRES_LMNOP_USER_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'lmnop',
+            'USER': 'lmnop',
+            'PASSWORD': os.environ['POSTGRES_LMNOP_USER_PASSWORD'],
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
