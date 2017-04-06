80470f379d19af45ca5b92c9d2d61351a34cf363
Larry Lade <lplade@users.noreply.github.com>
2017-04-06 00:18:44 -0500

    extend to collect static files

--- LMNOPsite/LMNOPsite/settings.py
+++ LMNOPsite/LMNOPsite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 
 # Where to send user after successful login if no other page is provided.
 # Should provide the user object.
