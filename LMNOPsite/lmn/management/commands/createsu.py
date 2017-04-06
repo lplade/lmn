b1712d6a1d103a2a541d2f97741ccd73b4e1aedf
Larry Lade <lplade@users.noreply.github.com>
2017-04-06 00:26:50 -0500

    trying to fix AWS errors

--- LMNOPsite/lmn/management/commands/createsu.py
+++ LMNOPsite/lmn/management/commands/createsu.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exist():
+        if not User.objects.filter(username='admin').exists():
             # Make sure to change the password promptly!
             User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
