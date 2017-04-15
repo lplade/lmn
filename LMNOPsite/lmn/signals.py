b87c9374346330db28ae2a425edf6778cc24befe
Larry Lade <lplade@users.noreply.github.com>
2017-04-15 11:11:34 -0500

    Twitter integration works

--- LMNOPsite/lmn/signals.py
+++ LMNOPsite/lmn/signals.py
@@ -9,7 +9,8 @@ from .models import Note
 @receiver(post_save, sender=Note)
 def post_to_twitter(sender, instance, created, **kwargs):
     # kwargs['instance'] is the Note object in questions
-    note = kwargs['instance']
+    # note = kwargs['instance']
+    note = instance
     # only tweet on creation, not updates or deletions
     if created:
         tweet(note.text)  # TODO error handling
